dcfdb2667f854564fd424c063b82df4b1939ff57
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-09-28 11:57:23 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int t;
+	cin >> t;
+	while(t--) {
+		int n;
+		cin >> n;
+		int a[n];
+		for(int i=0; i<n; ++i)
+			cin >> a[i];
+		cout << *min_element(a, a+n) << "\n";
+	}
+}
